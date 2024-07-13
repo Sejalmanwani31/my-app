2bfd5d7903005155748c09ccb8f821c8f2fc2eb3
email <manwani.sejal31@gmail.com>
2024-07-13 14:29:39 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -8,20 +8,33 @@ import Alert from './components/alert';
 
 function App() {
   const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message , type) => {
+    setAlert({
+      msg: message,
+      type: type 
+    })
+    setTimeout(() => {
+      setAlert(null);
+    },1500);
+  }
   const toggleMode =()=>{
     if(mode === 'light'){
       setMode('dark');
       document.body.style.backgroundColor = '#090a3f';
+      showAlert("Dark mode enabled" , "success") ;
     }
     else{
       setMode('light')
       document.body.style.backgroundColor = 'white';
+      showAlert("Light mode enabled" , "success") ;
     }
   }
   return (
     <>
    <Navbar title="TextUtils" mode={mode} toggleMode = {toggleMode}/>
-   <Alert alert = "This is sejal's site"/>
+   <Alert alert = {alert}/>
    <div className="container my-3">
    <Textform heading ="Enter The Text" mode={mode}/>
    {/* <About/> */}
