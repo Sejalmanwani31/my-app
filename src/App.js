a948e97ffa74d99d4911cd209379ee0835f78ea6
email <manwani.sejal31@gmail.com>
2024-12-28 13:24:39 +0530

    restart

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 // import logo from './logo.svg';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
+import About from './components/About';
 import { useState } from 'react';
 import './App.css';
 import Alert from './components/alert';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 // import About from './components/About';
 
 function App() {
@@ -33,12 +41,20 @@ function App() {
   }
   return (
     <>
+  <Router>
    <Navbar title="TextUtils" mode={mode} toggleMode = {toggleMode}/>
    <Alert alert = {alert}/>
    <div className="container my-3">
-   <Textform heading ="Enter The Text" mode={mode}/>
-   {/* <About/> */}
+   <Routes>
+          <Route exact path="/about" element={<About />}>
+          </Route>
+          <Route exact path="/" element = {<Textform showalert = {showAlert} heading ="Enter The Text" mode={mode}/>}>
+
+          </Route>
+        </Routes>
+  
    </div>
+   </Router>
  
 
    </>
